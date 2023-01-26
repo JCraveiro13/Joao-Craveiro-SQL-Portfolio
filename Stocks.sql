e4e4f94f9d4e856f40c7967a8acf9840ee757c5f
Joao <joaocraveiro1999@gmail.com>
2023-01-26 10:25:07 +0000

    Committing 3 SQL Projects into the Portfolio

---
+++ Stocks.sql
@@ -0,0 +1,9 @@
+CREATE TABLE stocks (
+symbol TEXT,
+name TEXT,
+datetime TEXT,
+price INTEGER
+);
+
+INSERT INTO stocks (symbol, name, datetime, price)
+VALUES ('TSLA', 'Tesla', '03/01/2022', 399.93);
